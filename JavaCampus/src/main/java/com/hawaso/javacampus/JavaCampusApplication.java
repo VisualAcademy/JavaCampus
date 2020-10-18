08aa8307a184de0d296de84d27272efa27b281e1
JavaCampus <dev@javacampus.com>
2020-10-18 00:16:03 +0000

    CreatorController

--- JavaCampus/src/main/java/com/hawaso/javacampus/JavaCampusApplication.java
+++ JavaCampus/src/main/java/com/hawaso/javacampus/JavaCampusApplication.java
@@ -2,12 +2,22 @@ package com.hawaso.javacampus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class JavaCampusApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(JavaCampusApplication.class, args);
 	}
 
+	@RestController	
+	@RequestMapping("/api")
+	public class CreatorController {
+		@GetMapping("/creator")
+		public String getCreator() {
+			return "RedPlus";
+		}
+	}
 }
