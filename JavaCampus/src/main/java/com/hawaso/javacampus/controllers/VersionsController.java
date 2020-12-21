dccc6ec6f698eb87b3a56b1d4ae0174e4afc7991
RedPlus <redplus@redplus.net>
2020-12-21 15:46:57 +0900

    CrossOrigin

--- JavaCampus/src/main/java/com/hawaso/javacampus/controllers/VersionsController.java
+++ JavaCampus/src/main/java/com/hawaso/javacampus/controllers/VersionsController.java
@@ -5,12 +5,14 @@ import java.util.List;
 
 import com.hawaso.javacampus.models.Version;
 
+//import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/versions")
+//@CrossOrigin(origins = "http://localhost:3000")
 public class VersionsController {
     @GetMapping
     public List<Version> getAll() {
