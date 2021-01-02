c36e7a3e09f1c84066d5376409868a6cc1a5a447
RedPlus <redplus@redplus.net>
2021-01-02 10:00:16 +0900

    PriceController

---
+++ JavaCampus/src/main/java/com/hawaso/javacampus/controllers/prices/PriceController.java
@@ -0,0 +1,12 @@
+package com.hawaso.javacampus.controllers.prices;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PriceController {
+    @GetMapping("/price")
+    public String index() {
+        return "/prices/pricing";
+    }
+}
