f49149101435324cd6d63c03fbbf4cec476afdc5
RedPlus <redplus@redplus.net>
2021-04-25 21:36:58 +0900

    Code Cleanup

--- JavaCampus/src/main/java/com/hawaso/javacampus/JavaCampusApplication.java
+++ JavaCampus/src/main/java/com/hawaso/javacampus/JavaCampusApplication.java
@@ -12,12 +12,22 @@ public class JavaCampusApplication {
 		SpringApplication.run(JavaCampusApplication.class, args);
 	}
 
+	//[!] 인라인으로 REST API 만들고 실행하기
+	//[1] /api/creator 경로 요청하면 "RedPlus" 문자열 출력
 	@RestController	
 	@RequestMapping("/api")
 	public class CreatorController {
 		@GetMapping("/creator")
 		public String getCreator() {
 			return "RedPlus";
+		}
+	}
+	//[2] /api/name 경로 요청하면 "박용준" 문자열 출력
+	@RestController
+	public class NameController {
+		@RequestMapping("/api/name")
+		public String getName() {
+			return "박용준";
 		}
 	}
 }
