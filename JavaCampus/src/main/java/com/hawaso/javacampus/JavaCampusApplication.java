3c2f79d31e81d9df1250666ec3b8aaf318d7e242
RedPlus <redplus@redplus.net>
2021-05-04 04:42:17 +0900

    Code Cleanup

--- JavaCampus/src/main/java/com/hawaso/javacampus/JavaCampusApplication.java
+++ JavaCampus/src/main/java/com/hawaso/javacampus/JavaCampusApplication.java
@@ -2,11 +2,13 @@ package com.hawaso.javacampus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+//public class JavaCampusApplication extends SpringBootServletInitializer {
 public class JavaCampusApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JavaCampusApplication.class, args);
