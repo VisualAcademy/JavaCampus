144913b0231c92e63dab5086a3a56b778b6f33ce
RedPlus <redplus@redplus.net>
2021-09-15 17:57:00 +0900

    Code Cleanup

--- JavaCampus/src/main/java/com/hawaso/javacampus/controllers/IndexController.java
+++ JavaCampus/src/main/java/com/hawaso/javacampus/controllers/IndexController.java
@@ -3,10 +3,11 @@ package com.hawaso.javacampus.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+// start https://www.javacampus.com 사이트의 메인 페이지 
 @Controller
 public class IndexController {
-    @GetMapping(value="/")
+    @GetMapping(value = "/")
     public String index() {
         return "index";
-   }
+    }
 }
