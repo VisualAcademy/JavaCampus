69f5b19f99e54d22164eca9ac73311584829657b
RedPlus <redplus@redplus.net>
2021-09-18 12:48:16 +0900

    Code Cleanup

--- JavaCampus/src/main/java/com/hawaso/javacampus/controllers/TodosController.java
+++ JavaCampus/src/main/java/com/hawaso/javacampus/controllers/TodosController.java
@@ -7,6 +7,7 @@ import com.hawaso.javacampus.models.Todo;
 import com.hawaso.javacampus.repositories.TodoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,8 +16,10 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+//@RequestMapping("/api/v1/todos") // 버전 지정 가능
 @RestController
 @RequestMapping("/api/todos")
 public class TodosController {
@@ -25,29 +28,37 @@ public class TodosController {
 
     // 입력
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED) // 201 Created.
     public Todo add(@RequestBody final Todo model) {
         return _todoRepository.saveAndFlush(model);
     }
+
     // 출력
     @GetMapping
     public List<Todo> getAll() {
         return _todoRepository.findAll(); 
     }
+
     // 상세보기
     @GetMapping
     @RequestMapping("{id}")
     public Optional<Todo> getById(@PathVariable Integer id) {
+        //return _todoRepository.getOne(id); // Optional<Todo> => Todo
         return _todoRepository.findById(id);
     }
+
     // 수정
     @PutMapping
     @RequestMapping(value = "{id}", method = RequestMethod.PUT)
     public Todo update(@PathVariable Integer id, @RequestBody Todo model) {
+        //var oldTodo = _todoRepository.getById(id);
         var oldModel = _todoRepository.getOne(id); 
         oldModel.setTitle(model.getTitle());
         oldModel.setIsDone(model.getIsDone());
+        //BeanUtils.copyProperties(model, oldModel, "id"); // 더 편한 방법 제공
         return _todoRepository.saveAndFlush(oldModel); 
     }
+
     // 삭제
     @DeleteMapping
     @RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
