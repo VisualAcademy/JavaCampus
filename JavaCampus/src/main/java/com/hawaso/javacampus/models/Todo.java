8141b4901d86efe651a6e63f7ad3dea06a2c5b97
RedPlus <redplus@redplus.net>
2021-09-18 10:48:27 +0900

    Todo Model Class

---
+++ JavaCampus/src/main/java/com/hawaso/javacampus/models/Todo.java
@@ -0,0 +1,34 @@
+// 모델 클래스 
+package com.hawaso.javacampus.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+// Todo 모델 클래스: Todos 테이블과 일대일로 매핑
+@Entity(name = "Todos")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+    private Boolean isDone;    
+    public Todo() { }
+    public Todo(String title, Boolean isDone) {
+        this.setTitle(title);
+        this.setIsDone(isDone);
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }    
+    public Boolean getIsDone() {
+        return isDone;
+    }
+    public void setIsDone(Boolean isDone) {
+        this.isDone = isDone;
+    }
+}
