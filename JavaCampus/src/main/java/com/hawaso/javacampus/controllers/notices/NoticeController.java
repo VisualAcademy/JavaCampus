f0f867588e7b506352f6f13b712903f4ea643a25
VisualAcademy <redplus@redplus.net>
2021-09-21 08:28:40 +0900

    NoticeController 클래스와 관련된 View 페이지 연결

--- JavaCampus/src/main/java/com/hawaso/javacampus/controllers/notices/NoticeController.java
+++ JavaCampus/src/main/java/com/hawaso/javacampus/controllers/notices/NoticeController.java
@@ -1,5 +1,30 @@
 package com.hawaso.javacampus.controllers.notices;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/notice")
 public class NoticeController {
-    
+    @GetMapping(value = {"", "/index"})    
+    public String index() {
+        return "views/notices/index";
+    }
+    @GetMapping(value = "/create")    
+    public String create() {
+        return "views/notices/create";
+    }
+    @GetMapping("/details")    
+    public String details() {
+        return "views/notices/details";
+    }
+    @GetMapping("/edit")    
+    public String edit() {
+        return "views/notices/edit";
+    }
+    @GetMapping("/delete")    
+    public String delete() {
+        return "views/notices/delete";
+    }
 }
