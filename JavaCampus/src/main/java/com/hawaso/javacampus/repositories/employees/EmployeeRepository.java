b3bfc79ba3f19c8a68bfa8880ce38287c0a54a02
Park YongJune <redplus@redplus.net>
2022-01-31 12:31:00 +0900

    .

--- JavaCampus/src/main/java/com/hawaso/javacampus/repositories/employees/EmployeeRepository.java
+++ JavaCampus/src/main/java/com/hawaso/javacampus/repositories/employees/EmployeeRepository.java
@@ -5,7 +5,9 @@ import java.util.List;
 import com.hawaso.javacampus.models.employees.Employee;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository // 이미 CrudRepository를 상속하기에 생략해도 되지만, 명확하게 리포지토리 클래스를 표현하기 위해서
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     public List<Employee> findByOrderByIdDesc(); 
     public List<Employee> findByOrderByFirstNameAsc(); 
--- JavaCampus/src/main/java/com/hawaso/javacampus/repositories/notices/INoticeRepository.java
+++ JavaCampus/src/main/java/com/hawaso/javacampus/repositories/notices/INoticeRepository.java
@@ -5,7 +5,9 @@ import java.util.List;
 import com.hawaso.javacampus.models.notices.Notice;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository // 이미 CrudRepository를 상속하기에 생략해도 되지만, 명확하게 리포지토리 클래스를 표현하기 위해서
 public interface INoticeRepository extends JpaRepository<Notice, Integer> {
     public List<Notice> findByOrderByIdDesc(); 
     public List<Notice> findByOrderByNameAsc(); 
