7855f33ebc974c9478b3886e1dc81573663aab8e
Park YongJune <redplus@redplus.net>
2022-02-02 17:48:28 +0900

    .

--- JavaCampus/src/main/java/com/hawaso/javacampus/repositories/campus/StudentRepository.java
+++ JavaCampus/src/main/java/com/hawaso/javacampus/repositories/campus/StudentRepository.java
@@ -1,5 +1,7 @@
 package com.hawaso.javacampus.repositories.campus;
 
+import java.util.List;
+
 import com.hawaso.javacampus.models.campus.Student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +9,12 @@ import org.springframework.stereotype.Repository;
 
 @Repository // 생략 가능 
 public interface StudentRepository extends JpaRepository<Student, Integer> {
+    // Select * From Students Where FirstName = ? 
+    List<Student> findByFirstName(String firstName);
     
+    // Select * From Students Where FirstName = ? And LastName = ?
+    List<Student> findByFirstNameAndLastName(String firstName, String lastName);
+    
+    // Select * From Students Where Upper(FirstName) = Upper(?)
+    List<Student> findByFirstNameIgnoreCase(String string);
 }
--- JavaCampus/src/test/java/com/hawaso/javacampus/repositories/StudentRepositoryTests.java
+++ JavaCampus/src/test/java/com/hawaso/javacampus/repositories/StudentRepositoryTests.java
@@ -2,6 +2,8 @@ package com.hawaso.javacampus.repositories;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
+
 import com.hawaso.javacampus.models.campus.Student;
 import com.hawaso.javacampus.repositories.campus.StudentRepository;
 
@@ -16,19 +18,35 @@ public class StudentRepositoryTests {
 
     @Test
     public void getAllTest() {
+        //[0] 처음 데이터 입력
         final Student student = new Student();
 
         student.setFirstName("YJ");
         student.setLastName("Park");
 
-        // 처음 데이터 입력
         _repository.save(student);
 
+        //[1] 1개의 데이터 저장 테스트 
         assertEquals(1, _repository.findAll().size());
 
-        // 현재 데이터 삭제
-        _repository.deleteById(student.getId());
+        //[3] 파생 쿼리 테스트
+        //[3][1] 이름만 테스트
+        List<Student> students1 = _repository.findByFirstName("YJ");
+        assertEquals(1, students1.size());
 
+        //[3][2] 이름과 성 모두 테스트 
+        List<Student> students2 = 
+            _repository.findByFirstNameAndLastName("YJ", "Kim");
+        assertEquals(0, students2.size());
+
+        //[3][3] 대소문자 구분
+        List<Student> students3 = _repository.findByFirstName("yj");
+        assertEquals(0, students3.size()); // YJ가 아닌 yj는 못 찾음
+        List<Student> students4 = _repository.findByFirstNameIgnoreCase("yj");
+        assertEquals(1, students4.size()); // YJ가 아닌 yj는 못 찾음
+
+        //[2] 현재 데이터 삭제
+        _repository.deleteById(student.getId());
         assertEquals(0L, _repository.count());
     }
 }
