88e6c56c4f4443736f45264379022e17d5717823
Park YongJune <redplus@redplus.net>
2022-02-05 16:28:31 +0900

    Model 클래스에 애너테이션 추가

--- JavaCampus/src/main/java/com/hawaso/javacampus/models/buffets/Noodle.java
+++ JavaCampus/src/main/java/com/hawaso/javacampus/models/buffets/Noodle.java
@@ -1,6 +1,14 @@
 package com.hawaso.javacampus.models.buffets;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Noodle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id; 
 
     private String name;
