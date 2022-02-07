4be2a1da391ec72cb154cfe338a29785ed257645
Park YongJune <redplus@redplus.net>
2022-02-07 18:49:01 +0900

    .

--- JavaCampus/src/main/java/com/hawaso/javacampus/controllers/GreetingController.java
+++ JavaCampus/src/main/java/com/hawaso/javacampus/controllers/GreetingController.java
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 	@GetMapping("/greeting")
-	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+	public String greeting(
+			@RequestParam(name = "name", required = false, defaultValue = "World") String name, // 쿼리스트링 받기
+			Model model // 컨트롤러에서 뷰 페이지로 데이터 전송
+	) {
 		model.addAttribute("name", name);
 		return "greeting";
 	}
