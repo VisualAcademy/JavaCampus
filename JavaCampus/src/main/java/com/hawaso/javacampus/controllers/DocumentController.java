81a738f10b4e4ab2ab4e03c1c56282f226163d6b
Park YongJune <redplus@redplus.net>
2022-09-21 04:44:15 +0900

    Controller to RestController

--- JavaCampus/src/main/java/com/hawaso/javacampus/controllers/DocumentController.java
+++ JavaCampus/src/main/java/com/hawaso/javacampus/controllers/DocumentController.java
@@ -1,16 +1,22 @@
 package com.hawaso.javacampus.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.hawaso.javacampus.models.Document;
 
-@Controller
+@RestController
 public class DocumentController {
-    @ResponseBody
     @GetMapping("/document")
     public Document index() {
         return new Document(1234, "my document");
     }
 }
+// @Controller
+// public class DocumentController {
+//     @ResponseBody
+//     @GetMapping("/document")
+//     public Document index() {
+//         return new Document(1234, "my document");
+//     }
+// }
