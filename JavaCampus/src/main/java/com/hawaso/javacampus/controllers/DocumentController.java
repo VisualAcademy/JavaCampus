fffe016d29d520d6a151d93facfac1ede37ad2a2
Park YongJune <redplus@redplus.net>
2022-09-21 05:15:41 +0900

    Document post action method

--- JavaCampus/src/main/java/com/hawaso/javacampus/controllers/DocumentController.java
+++ JavaCampus/src/main/java/com/hawaso/javacampus/controllers/DocumentController.java
@@ -1,6 +1,8 @@
 package com.hawaso.javacampus.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hawaso.javacampus.models.Document;
@@ -8,8 +10,15 @@ import com.hawaso.javacampus.models.Document;
 @RestController
 public class DocumentController {
     @GetMapping("/document")
-    public Document index() {
+    public Document get() {
         return new Document(1234, "my document");
+    }
+    @PostMapping("/document")
+    public Document post(@RequestBody Document document) {
+
+        document.setContent(document.getContent() + " saved.");
+
+        return document; 
     }
 }
 // @Controller
