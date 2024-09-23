cf4708cf88de0baadeafee10ca118ae3c0ecde6b
Park YongJune <redplus@redplus.net>
2024-09-23 16:59:20 +0900

    .

--- JavaCampus/src/main/java/com/hawaso/javacampus/models/vendors/Vendor.java
+++ JavaCampus/src/main/java/com/hawaso/javacampus/models/vendors/Vendor.java
@@ -1,5 +1,13 @@
 package com.hawaso.javacampus.models.vendors;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Vendor {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
