3e38212c04c70a571f39c5f3a5d13c62830652e7
Park YongJune <redplus@redplus.net>
2025-03-14 10:48:35 +0900

    action method is handler method

--- JavaCampus/src/main/java/com/hawaso/javacampus/controllers/GreetingController.java
+++ JavaCampus/src/main/java/com/hawaso/javacampus/controllers/GreetingController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 // Greeting 이름으로 컨트롤러 만들기 
 @Controller
 public class GreetingController {
-	// greeting 이름으로 액션 메서드 만들기
+	// greeting 이름으로 액션 메서드(핸들러 메서드) 만들기
 	@GetMapping("/greeting")
 	public String greeting(
 			@RequestParam(name = "name", required = false, defaultValue = "World") String name, // 쿼리스트링 받기
